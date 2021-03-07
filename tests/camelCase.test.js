a866bc24758d6ccf613df6aa50399cbca019c65c
lenny alimwa <lensig13@gmail.com>
2021-03-07 16:53:55 +0300

    ch-simple-formatting

--- tests/camelCase.test.js
+++ tests/camelCase.test.js
@@ -4,4 +4,5 @@ const leia = new Leia();
 
 it('should convert a stroke delimited string to camelCase', () => {
 	expect(leia.camelCase("font-size")).toBe("fontSize");
+	expect(leia.camelCase("border-radius-left")).toBe("borderRadiusLeft");
 });
