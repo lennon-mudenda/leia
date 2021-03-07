a5895b8e7754c866e7844a88e348ce5ae9e5c55f
lenny alimwa <lensig13@gmail.com>
2021-03-07 14:36:25 +0300

    ft-add-base-leia-class-template

---
+++ src/core/Leia.js
@@ -0,0 +1,34 @@
+const css = require('css')
+
+export default class Leia
+{
+	constructor()
+	{
+
+	}
+
+	parseString()
+	{
+
+	}
+
+	parseFile()
+	{
+
+	}
+
+	parse()
+	{
+
+	}
+
+	camelCase()
+	{
+
+	}
+
+	validateProperty()
+	{
+
+	}
+}
